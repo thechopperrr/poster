d1fc17ac5d1619763a26a37f025224ab0c4e41d4
AndreyNikolaev <you@example.com>
2016-03-13 11:40:30 +0200

    Initial Commit

---
+++ Poster/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Poster
+//
+//  Created by Interns on 3/13/16.
+//  Copyright © 2016 com.omg-m. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
